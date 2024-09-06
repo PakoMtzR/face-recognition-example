98421b19494af176b0005117df894417d8d9df57
PakoMtzR <francisco.2k1@gmail.com>
2024-09-06 17:29:36 -0600

    detencion de cara en tiempo real y documento readme

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import cv2
 import face_recognition as fr
 import os
 import numpy as np
+from pathlib import Path
 
-path = "persons"
+path = Path(os.getcwd(), "persons")  # "persons"
 persons_images = []
 persons_names = []
 persons_list = os.listdir(path)
 encoded_person_list = []
 
+# Obtenemos la lista de personas y sus imagenes
 for name in persons_list:
     # Leemos imagen y lo agregamos en la lista de imagenes
     image = cv2.imread(f"{path}\\{name}")
@@ -19,44 +21,46 @@ for name in persons_list:
     persons_names.append(os.path.splitext(name)[0])
 
     # Codificamos imagen y lo agregamos a una lista
-    image_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    encoded_image = fr.face_encodings(image_RGB)[0]
-    encoded_person_list.append(encoded_image)
+    image_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Cambiamos espacio de color a RGB
+    encoded_image = fr.face_encodings(image_RGB)[0]     # Codificamos la cara
+    encoded_person_list.append(encoded_image)           # Lo guardamos en una lista
 
+# Imprimimos por consola los nombres de las personas en el banco de imagenes
+print("Registered persons:")
+print("-"*30)
+for i,person in enumerate(persons_names):
+    print(f"{i+1}\t{person}")
+print("-"*30)
 
-print(persons_names)
+# Webcam
+camara = cv2.VideoCapture(0)
 
-# encoded_person_list = encode(persons_images)
-
-# Capturamos una imagen con la webcam
-capture = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-success, image_captured = capture.read()
-
-if not success:
-    print("No se pudo hacer la captura")
-else:
-    # cv2.imshow("test", image_captured)
-    capture_face = fr.face_locations(image_captured)
-    encoded_capture_face = fr.face_encodings(image_captured, capture_face)
+while True:
+    success, frame = camara.read()
     
+    capture_face = fr.face_locations(frame)
+    encoded_capture_face = fr.face_encodings(frame, capture_face)
+
     for face, location_face in zip(encoded_capture_face, capture_face):
         # Comparamos medidas con las imagenes
         matches = fr.compare_faces(encoded_person_list, face)
         distances = fr.face_distance(encoded_person_list, face)
-        print(distances)
+        # print(distances)
 
         # Buscamos con quien de las imagenes guardadas coincide mas
         match_index = np.argmin(distances)  
-        if distances[match_index] > 0.6:
-            print("No existen coincidencias")
-        else:
+        if distances[match_index] <= 0.6:
             person_name = persons_names[match_index]
 
             y1, x2, y2, x1 = location_face
-            cv2.rectangle(image_captured, (x1,y1), (x2,y2), (0,255,0), 2)
-            cv2.rectangle(image_captured, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
-            cv2.putText(image_captured, person_name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+            cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 2)
+            cv2.rectangle(frame, (x1,y2-35), (x2,y2), (0,255,0), cv2.FILLED)
+            cv2.putText(frame, person_name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
 
-            cv2.imshow("Image", image_captured)
+    cv2.imshow("Camara (press 'q' to close)", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
     
-        cv2.waitKey(0)
+camara.release()
+cv2.destroyAllWindows()
